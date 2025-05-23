b244f750209b4a70908673bcc27563ce3fec9be1
230660221014 <230660221014@student.unsap.ac.id>
2025-05-23 21:14:24 +0700

    Menambahkan Praktikum 1, 2, & 3 dan portfolio

---
+++ 230660221014/pratikum 3/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Tunggu hingga dokumen selesai dimuat
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.getElementById("myButton");
+
+  button.addEventListener("click", function () {
+    alert("Tombol diklik!");
+  });
+});
