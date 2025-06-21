b15cfe82078d5c19056b16d51cd65aa729be8946
230660221103 <cacareina6@gmail.com>
2025-06-21 17:25:14 +0700

    menambahkan react dengan vite

---
+++ 230660221103/Praktikum-react/latihan-react-vite/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'; // pastikan ini ada untuk menghubungkan dengan App.css
+
+function App() {
+  return (
+    <div>
+      <h1 className="title">Reina Anatasya Kusumah Putri</h1>
+      <p>Saya adalah Mahasiswa Jurusan Sistem Informasi Fakultas Teknologi Informasi Universitas Sebelas April</p>
+    </div>
+  );
+}
+
+export default App;
